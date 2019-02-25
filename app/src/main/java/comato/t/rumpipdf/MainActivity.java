23e0814e9527a8d8a3d596583b6c218399d21e91
tomi <tomi.firman@student.unri.ac.id>
2019-02-25 11:33:26 +0700

    Initial commit

---
+++ app/src/main/java/comato/t/rumpipdf/MainActivity.java
@@ -0,0 +1,31 @@
+package comato.t.rumpipdf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        PDFView pdfView = findViewById(R.id.pdfView);
+
+
+        pdfView.fromAsset("rumpiit.pdf")
+                .enableSwipe(true) // allows to block changing pages using swipe
+                .swipeHorizontal(false)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                // allows to draw something on the current page, usually visible in the middle of the screen
+                .enableAnnotationRendering(false) // render annotations (such as comments, colors or forms)
+                .password(null)
+                .scrollHandle(null)
+                .enableAntialiasing(true) // improve rendering a little bit on low-res screens
+                // spacing between pages in dp. To define spacing color, set view background
+                .spacing(0)
+                .load();
+    }
+}
